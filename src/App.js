95a0a62b95c25a52333d95637115d03cf7c9ea5e
Augusta <augustajohn93@gmail.com>
2023-12-18 15:02:50 -0600

    word game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { getRandomWord } from './SampleWords';
 import './App.css';
 
 function App() {
+  const [word, setWord] = useState(getRandomWord());
+  const [hint, setHint] = useState(3);
+  const [chosen, setChosen] = useState([]);
+  const [message, setMessage] = useState();
+  const [displayAnswer, setDisplayAnswer] = useState(false);
+  const [wrongGuess, setWrongGuess] = useState(0);
+
+  function getHint() {
+    if (hint > 0) {
+      const letterIndex = Array.from(word.word).findIndex((letter) => !chosen.includes(letter));
+      setChosen([...chosen, word.word[letterIndex]]);
+      setHint(hint - 1);
+    } 
+  }
+
+  function onRestart() {
+    setWord(getRandomWord());
+    setHint(3);
+    setChosen([]);
+    setMessage();
+    setDisplayAnswer(false);
+    setWrongGuess(0);
+  }
+
+  function displayLetters() {
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    const letterButtons = Array.from(letters).map((letter, index) => (
+      <button className='m-1.5 px-3.5 py-2.5 text-base text-white font-bold rounded bg-[#2196f3] disabled:bg-[#ccc]'
+        onClick={() => selectButton(letter)} disabled={chosen.includes(letter)}
+        key={index}>{letter}</button>
+    ))
+
+    return letterButtons;
+  }
+
+  function selectButton(letter) {
+    if(!chosen.includes(letter)) {
+      setChosen(chosen => [...chosen, letter]);
+    }
+    if(!word.word.includes(letter)) {
+      setWrongGuess(wrongGuess+1);
+    }
+  }
+
+  function removeLetter() {
+    setChosen(chosen.slice(0, -1));
+  }
+
+  function guessWord() {
+    const guess = Array.from(word.word).every((letter) => chosen.includes(letter));
+
+    if (guess) {
+      setMessage("Congo Geek! You have guessed the word correctly!");
+    } else {
+      setMessage("You made a Wrong Guess Geek!. Try again!");
+      setDisplayAnswer(true);
+    }
+  }
+
+  useEffect(() => {
+    if(wrongGuess === 3) {
+      alert('Game Over! You made too many wrong guesses');
+      onRestart();
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col justify-center items-center bg-[#f5f5f5] min-h-screen'>
+      <h1 className='font-bold text-4xl mb-8 text-[#15e402]'>Word Guess Game</h1>
+      <div className='flex justify-center items-center mb-12'>
+        {Array.from(word.word).map((letter, index) => (
+          <div className='flex justify-center items-center w-14 h-14 rounded-full text-white bg-black opacity-100 font-bold text-2xl mx-1 transition-opacity duration-200 ease-in-out'
+            key={index}> {chosen.includes(letter) ? letter : ''} </div>
+        ))}
+      </div>
+      <p className='mb-5 font-bold text-lg text-black'>Hint : {word.description}</p>
+      {message &&
+        <div className='text-2xl font-bold text-[#333] text-center mb-5'>
+          <p>{message}</p>
+          {displayAnswer && <p>Correct word is: {word.word}</p>}
+        </div>
+      }
+      <div className='mb-8 self-start'>
+        <button className='commonButtons' onClick={onRestart}>Restart</button>
+        <button className='commonButtons' disabled={!chosen.length} onClick={removeLetter} >Remove Letter</button>
+      </div>
+      <div className='flex justify-center flex-wrap mb-8'>
+        {displayLetters()}
+      </div>
+      <div className='mb-5 font-bold text-xl text-[#333] self-start'>
+        Hints Remaining: {hint}{'  '}
+        <button className='commonButtons' disabled={hint === 0} onClick={getHint}>Get Hint</button>
+      </div>
+      <button className='commonButtons' disabled={!chosen.length} onClick={guessWord}>Guess</button>
     </div>
   );
 }
---
+++ src/SampleWords.js
@@ -0,0 +1,30 @@
+const sampleWords = [ 
+    { 
+        word: "HELLO", 
+        description: "A common greeting to say hi."
+    }, 
+    { 
+        word: "WORLD", 
+        description: "The planet we live on, which is full of land and water."
+    }, 
+    { 
+        word: "JAVASCRIPT", 
+        description: "A popular programming language for building interactive websites and provides behaviour to applications."
+    }, 
+    { 
+        word: "REACT", 
+        description: "A Javascript library in which we have written this project code"
+    }, 
+    { 
+        word: "PROGRAMMING", 
+        description: "The process of developing code to assist computers to perform tasks."
+    }, 
+    { 
+        word: "GEEKSFORGEEKS", 
+        description: "An educational website for computer science 'geeks.'"
+    } 
+];
+
+export function getRandomWord() {
+    return sampleWords[Math.floor(Math.random() * sampleWords.length)];
+}
